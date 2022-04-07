6ab61ea4651fe31aae7fcec909dc96bf2dd0b769
Mikko Huotari <mikko.huotari@solita.fi>
2022-04-07 08:36:55 +0300

    freaky 8-bit look

--- script.js
+++
@@ -1,7 +0,0 @@
-var adjectives = ["rad", "🔥", "chill", "ok", "🌊", "loopy"];
-var random = Math.round(Math.random() * (adjectives.length - 1));
-var randomAdjective = adjectives[random];
-
-window.onload = function(){
-  document.getElementById('adjective').innerHTML = this.randomAdjective;
-}
